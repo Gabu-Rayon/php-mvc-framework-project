6d213b1c6a05b6f8cbe490022e9635f69d632d50
Gabu-Rayon <gibsonmurimi4@gmail.com>
2023-05-10 17:05:49 +0300

    commits

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'autoload/namespacer',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'a090730ea20f626aa1312e7b958b094f5b5b5164',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'autoload/namespacer' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'a090730ea20f626aa1312e7b958b094f5b5b5164',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
