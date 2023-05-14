29228aeb36b62d6c95bbc9758a27f20bdf3acb5c
Gabu-Rayon <gibsonmurimi4@gmail.com>
2023-05-14 21:20:57 +0300

    commits

--- core/Application.php
+++ core/Application.php
@@ -22,7 +22,7 @@ class Application{
         self::$app = $this; 
         $this->request = new Request();
         $this->response = new Response();
-         //to handle response and Request
+         //to handle response & Request
         $this->router = new Router($this->request,$this->response);
        
     }
