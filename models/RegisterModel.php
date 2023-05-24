91106bf20dce1115c284e1bd41c6be95f68c2cdb
Gabu-Rayon <gibsonmurimi4@gmail.com>
2023-05-24 19:56:05 +0300

    commit

--- models/RegisterModel.php
+++ models/RegisterModel.php
@@ -8,11 +8,11 @@ use app\core\Model;
 
 class RegisterModel extends Model{
 
-    public string $firstname;
-    public string $lastname;
-    public string  $email;
-    public string $password;
-    public string $confirmPassword;
+    public string $firstname = '';
+    public string $lastname = '';
+    public string  $email = '';
+    public string $password  = '';
+    public string $confirmPassword = ''  ;
 
 
     public function register(){
--- views/register.php
+++ views/register.php
@@ -1,15 +1,21 @@
 <h1>Register</h1>
 
-<?php $form =  \app\core\form\Form::begin('',"post")  ?>
+<?php $form = \app\core\form\Form::begin('',"post")  ?>
 
 <?php  echo $form->field($model,'firstname') ?>
+
 <?php  echo $form->field($model,'lastname') ?>
+
 <?php  echo $form->field($model,'email') ?>
+
 <?php  echo $form->field($model,'password') ?>
+
 <?php  echo $form->field($model,'confirmPassword') ?>
+
 <button type="submit" class="btn btn-primary">Register</button>
 
 <?php echo \app\core\form\Form::end()  ?>
+
 
 <!-- <form action="" method="post">
     <div class="row">
