0894081f4b9babfdacd337b82eceaa3918ec7fc8
Gabu-Rayon <gibsonmurimi4@gmail.com>
2023-05-25 15:55:43 +0300

    commit

--- views/register.php
+++ views/register.php
@@ -1,13 +1,18 @@
 <?php 
 
+
 ?>
 <h1>Create an Account</h1>
 
 <?php $form = \app\core\form\Form::begin('',"post")?>
-
-<?php  echo $form->field($model,'firstname')?>
-
-<?php  echo $form->field($model,'lastname')?>
+<div class="row">
+    <div class="col">
+        <?php  echo $form->field($model,'firstname')?>
+    </div>
+    <div class="col">
+        <?php  echo $form->field($model,'lastname')?>
+    </div>
+</div>
 
 <?php  echo $form->field($model,'email')?>
 
