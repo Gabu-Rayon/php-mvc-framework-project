9aecf7918ab7c44ac066442859e6fafc57235bf4
Gabu-Rayon <gibsonmurimi4@gmail.com>
2023-05-25 17:17:57 +0300

    commit

--- views/register.php
+++ views/register.php
@@ -16,9 +16,9 @@
 
 <?php  echo $form->field($model,'email')?>
 
-<?php  echo $form->field($model,'password')?>
+<?php  echo $form->field($model,'password')->passwordField() ?>
 
-<?php  echo $form->field($model,'confirmPassword')?>
+<?php  echo $form->field($model,'confirmPassword')->passwordField() ?>
 
 <button type="submit" class="btn btn-primary">Register</button>
 
