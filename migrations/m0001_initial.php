73756dcf8c7647139f92a564ef003cf52294d782
Gabu-Rayon <gibsonmurimi4@gmail.com>
2023-05-26 11:28:35 +0300

    commit

---
+++ migrations/m0001_initial.php
@@ -0,0 +1,13 @@
+<?php
+class m0001_initial{
+ public function up()
+    {
+        echo "Applying Migrations";
+    }
+
+    public function down()
+    {
+       echo "Down Migrations";
+    }
+    
+}
