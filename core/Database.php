bb69d48c848c95d5d50c1c9f5d9f494e8acc11ac
Gabu-Rayon <gibsonmurimi4@gmail.com>
2023-05-26 15:38:59 +0300

    commit

--- core/Database.php
+++ core/Database.php
@@ -22,10 +22,38 @@ class Database{
 
      public function applyMigrations(){
         $this->createMigrationsTable();
-        $this->getAppliedMigrations();
+       $appliedMigrations =  $this->getAppliedMigrations();
 
+      $newMigrations = [];
         $files = scandir(Application::$ROOT_DIR . '/migrations');
-     }
+        $toApplyMigrations  = array_diff($files, $appliedMigrations);
+        foreach ($toApplyMigrations as $migration) {
+          if ($migration == '.' || $migration === '..') {
+            continue;
+          }
+          require_once Application::$ROOT_DIR.'/migrations/'.$migration;
+       
+         $className = pathinfo($migration, PATHINFO_FILENAME);
+       
+          $instance = new $className();
+       
+          $this->log("Applying migration $migration");
+
+         $instance->up();
+
+         $this->log( "Applied migration $migration");
+         
+         $newMigrations[] = $migration;
+        }
+        if (!empty($newMigrations)) {
+
+         $this->saveMigrations($newMigrations);
+
+        }else {
+        $this->log( "All migrations are Applied");
+        }
+   
+      }
 
      public function createMigrationsTable(){
         $this->pdo->exec("CREATE TABLE IF NOT EXISTS migrations(
@@ -43,6 +71,24 @@ class Database{
         $statement->execute();
 
         return $statement->fetchAll(\PDO::FETCH_COLUMN);
+    }
+    public function saveMigrations(array $migrations){
+ 
+      $str = implode(","  , array_map(fn($m) => "('$m')",$migrations));
+      
+     $statement =  $this->pdo->prepare("INSERT INTO migrations(migration) VALUES 
+                  
+                 $str;
+                 
+      ");
+        
+      $statement->execute();
+      
+    }
+
+    protected function log($message){
+      
+      echo '['.date('Y-m-d H:i:s').'] -'.$message. PHP_EOL;
     }
          
 }
