b9300b569c3797574c3cb4cc0ead56835053b7aa
Gabu-Rayon <gibsonmurimi4@gmail.com>
2023-06-02 16:56:10 +0300

    commit

--- core/Controller.php
+++ core/Controller.php
@@ -1,35 +1,40 @@
-<?php 
-
+<?php
 
 namespace app\core;
 
 use app\core\middlewares\BaseMiddleware;
 
-class Controller{
-
+class Controller
+{
     public string $layout = 'main';
-    
+
     public string $action = '';
-    
-    
-     /**
-      * @var \app\core\middlewares\BaseMiddleware
-      */
-      
-    public array $middlewares = [];
-    
-     public function setLayout($layout){
-        
-         $this->layout = $layout;
-     }
-    public function render($view,$params=[]){
-      
+
+
+    /**
+     * @var \app\core\middlewares\BaseMiddleware
+     */
+    protected array $middlewares = [];
+
+    public function setLayout($layout)
+    {
+        $this->layout = $layout;
+    }
+    public function render($view, $params=[])
+    {
         return Application::$app->router->renderView($view, $params);
     }
-    
-    public function registerMiddleware(BaseMiddleware $middleware ){
-       
-        $this->middlewares[] = $middleware; 
-   
+
+    public function registerMiddleware(BaseMiddleware $middleware)
+    {
+        $this->middlewares[] = $middleware;
+    }
+
+    /**
+     * @return \app\core\middlewares\BaseMiddleware[]
+     */
+    public function getMiddlewares(): array
+    {
+        return $this->middlewares;
     }
 }
--- core/Router.php
+++ core/Router.php
@@ -50,11 +50,25 @@ class Router
 
         }
 
-        if(is_array($callback)){
+        if (is_array($callback)) {
+            /**
+             * @var \app\core\Controller $controller
+             */
+            $controller = new $callback[0];
+
+            $controller->action = $callback[1];
+
+            Application::$app->controller = $controller;
+
+            $middlewares = $controller->getMiddlewares();
+
+            foreach ($middlewares as $middleware) {
+
+                $middleware->execute();
+
+            }
             
-            Application::$app->controller = new $callback[0]();
-            Application::$app->controller->action = $callback[1];
-            $callback[0] = Application::$app->controller;
+            $callback[0] = $controller;
         }
 
          return call_user_func($callback,$this->request,$this->response);
