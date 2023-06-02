a73a2e0162062b906259185d554462b6bbd7b0da
Gabu-Rayon <gibsonmurimi4@gmail.com>
2023-06-02 16:29:13 +0300

    commit

--- core/exception/ForbiddenException.php
+++ core/exception/ForbiddenException.php
@@ -4,6 +4,8 @@
 namespace app\core\exception;
 
 
-class ForbiddenException{
+class ForbiddenException extends \Exception{
     
+    protected $message = 'You don\'t have permission to access this page';
+    protected $code = 403;
 }
